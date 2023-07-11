5bba59c5f03e23be23afe7e3b9b397f326ea9361
kerlos-alrayan <kerlossheref@gmail.com>
2023-07-11 22:26:13 +0300

    commit

--- lib/repository/screens/intro_screen.dart
+++ lib/repository/screens/intro_screen.dart
@@ -1,16 +1,22 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:threads_app_set26/repository/screens/login_screen.dart';
+import 'package:threads_app_set26/repository/screens/main_screen.dart';
 
 class IntroScreen extends StatelessWidget {
   const IntroScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    if(FirebaseAuth.instance.currentUser == null){
-      return const LoginScreen();
-    }else {
-      return const Scaffold();
-    }
+   return StreamBuilder(
+       stream: FirebaseAuth.instance.authStateChanges(),
+       builder: (context, snapshot){
+         if(snapshot.data != null){
+           return const MainScreen();
+         }else{
+           return const LoginScreen();
+         }
+       }
+   );
   }
 }
